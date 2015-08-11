2b5f6bb08fe0be38c2744c44c379e1c072e0d2a3
chenfushan <chenfushan1992@gmail.com>
2015-08-11 23:51:21 +0800

    add EngWord and CreateDictForDouban 基础EngWord创建完成，创建字典的CreateDictForDouban刚写完构造函数需要设计一下逻辑

---
+++ crawler-for-douban/src/crawler/douban/dict/CreateDictForDouban.java
@@ -0,0 +1,14 @@
+package crawler.douban.dict;
+
+public class CreateDictForDouban {
+
+    private EngWord engWord;
+    
+    public CreateDictForDouban(){
+        engWord = new EngWord();
+    }
+    
+    
+    
+    
+}
---
+++ crawler-for-douban/src/crawler/douban/dict/EngWord.java
@@ -0,0 +1,65 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2015 All Rights Reserved.
+ */
+package crawler.douban.dict;
+
+import java.util.Arrays;
+
+/**
+ * @author alps
+ * @version $Id: EngWord.java,v 0.1 2015年8月11日 下午8:43:01 alps Exp $
+ */
+public class EngWord {
+    private String[] aplha = new String[52];
+    
+    /**
+     * constructor for EngWord
+     */
+    public EngWord(){
+        int i = 0;
+        for(; i < 26; i++){
+            aplha[i] = ""+('A'+i);
+        }
+        for(;i < 52;i++){
+            aplha[i] = ""+('a'-26+i);
+        }
+    }
+    
+    /**
+     * 获得aplha的某个值
+     * @param num
+     * @return
+     */
+    public String getElement(Integer num){
+        return aplha[num];
+    }
+
+    /**
+        * Getter method for property <tt>aplha</tt>.
+        * 
+        * @return property value of aplha
+        */
+    public String[] getAplha() {
+        return aplha;
+    }
+
+    /**
+        * Setter method for property <tt>aplha</tt>.
+        * 
+        * @param aplha value to be assigned to property aplha
+        */
+    public void setAplha(String[] aplha) {
+        this.aplha = aplha;
+    }
+
+    /**
+     * @return
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "EngWord [aplha=" + Arrays.toString(aplha) + "]";
+    }
+    
+}
