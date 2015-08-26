49ab072c8c47f3c224bf4a24d08d7e060ae3ec1c
chenfushan <chenfushan1992@gmail.com>
2015-08-26 14:31:54 +0800

    add file GlobalVariable and crawlerDouban

---
+++ crawler-for-douban/src/crawler/douban/global/GlobalVariable.java
@@ -0,0 +1,15 @@
+/**
+ * Alps and AskingWindy Inc.
+ * Copyright (c) 2004-2015 All Rights Reserved.
+ */
+package crawler.douban.global;
+
+/**
+ * @author alps
+ * @version $Id: GlobalVariable.java,v 0.1 2015年8月26日 上午10:38:17 alps Exp $
+ */
+public class GlobalVariable {
+    /** 爬虫爬取内容的前置URL */
+    public final static String PREURL = "http://www.douban.com/people/";
+    
+}
