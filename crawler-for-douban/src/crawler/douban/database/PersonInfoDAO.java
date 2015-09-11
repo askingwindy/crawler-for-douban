ac17f44a1c323a649138c793f172f0aa4ac2602b
chenfushan <chenfushan1992@gmail.com>
2015-09-11 16:32:33 +0800

    add PersonInfoDAO.java code

---
+++ crawler-for-douban/src/crawler/douban/database/PersonInfoDAO.java
@@ -0,0 +1,13 @@
+/**
+ * Alps and AskingWindy Inc.
+ * Copyright (c) 2004-2015 All Rights Reserved.
+ */
+package crawler.douban.database;
+
+/**
+ * @author alps
+ * @version $Id: PersonInfoDAO.java,v 0.1 2015年8月26日 下午6:48:06 alps Exp $
+ */
+public class PersonInfoDAO {
+
+}
