1a9df7a6cb3dbbba0c2f3e2fca826f8d48c98128
chenfushan <chenfushan1992@gmail.com>
2015-09-16 22:57:37 +0800

    20150916 重构完毕，下一步数据库操作

--- crawler-for-douban/src/crawler/douban/crawler/CrawlerDouban.java
+++ crawler-for-douban/src/crawler/douban/crawler/CrawlerDouban.java
@@ -92,195 +92,345 @@ public class CrawlerDouban {
      */
     private PersonInfo convertDoc2PersonInfo(Document doc) {
         PersonInfo info = new PersonInfo();
-        info.setUserId(id);
-        info.setHomePageUrl(url);
         if (doc == null) {
             return null;
         }
-        Elements nameHtml = doc.select("title");
-        //        Elements sexHtml = doc.select("sex"); //页面想好像没有性别
-        //        Elements ageHtml = doc.select("age"); //页面想暂时无法获取年龄
-        Elements joinDateHtml = doc.select("div[class=\"user-info\"] div[class=\"pl\"]");
-        Elements obssinHtml = doc.select("div[class=\"obssin\"");
-        Elements readingBookHtml = null;
-        Elements wantBookHtml = null;
-        Elements wantMovieHtml = null;
-        Elements watchedMovieHtml = null;
-        Elements listeningMusicHtml = null;
-        Elements wantMusicHtml = null;
-        Elements oftenGroupHtml = doc.select("dl[class=\"ob\"] dd a");
-        Elements friendHtml = doc.select("div[id=\"friend\"]");
-        Elements fellowPeopleNumHtml = null;
-        Elements fellowedPeopleNumHtml = null;
-        if (friendHtml != null) {
-            fellowPeopleNumHtml = friendHtml.select("a");
-            fellowedPeopleNumHtml = friendHtml.select("p[class=\"rev-link\"]");
 
-        } else {
-            info.setFellowedPeopleNum(null);
-            info.setFellowPeopleNum(null);
-            logger.infoLog("empty fellow and fellowed");
+        Elements obssinHtml = doc.select("div[class=\"obssin\"");
+
+        //用户id
+        info.setUserId(id);
+        //用户主页url
+        info.setHomePageUrl(url);
+
+        //关注的人数
+        String fellowPeopleNum = getFellowPeopleNum(doc);
+        info.setFellowPeopleNum(fellowPeopleNum);
+        if (fellowPeopleNum == null) {
+            logger.infoLog("empty fellow people");
         }
-        if (obssinHtml != null) {
-            for (Element books : obssinHtml) {
-                Elements substatus = books.select("div[class=\"substatus\"]");
-                if (substatus.text().equals("在读")) {
-                    readingBookHtml = books.select("li[class=\"aob\"] a img");
-                }
-                if (substatus.text().equals("想读")) {
-                    wantBookHtml = books.select("li[class=\"aob\"] a img");
-                }
-                if (substatus.text().equals("想看")) {
-                    wantMovieHtml = books.select("li[class=\"aob\"] a");
-                }
-                if (substatus.text().equals("看过")) {
-                    watchedMovieHtml = books.select("li[class=\"aob\"] a");
-                }
-                if (substatus.text().equals("在听")) {
-                    listeningMusicHtml = books.select("li[class=\"aob\"] a");
-                }
-                if (substatus.text().equals("想听")) {
-                    wantMusicHtml = books.select("li[class=\"aob\"] a");
-                }
-            }
-        } else {
-            logger.infoLog("empty book");
-        }
-        //关注人数
-        if (fellowPeopleNumHtml != null) {
-            String fellowPeopleNum = UtilsMethod.findFirstStringByRegex("成员[0-9]+",
-                fellowPeopleNumHtml.text());
-            if (fellowPeopleNum != null) {
-                fellowPeopleNum = fellowPeopleNum.replaceAll("[\\D]+", "");
-                if (fellowPeopleNum != null) {
-                    info.setFellowPeopleNum(fellowPeopleNum);
-                } else {
-                    info.setFellowPeopleNum(null);
-                }
-            } else {
-                info.setFellowPeopleNum(null);
-            }
-        } else {
-            info.setFellowPeopleNum(null);
-        }
-        //被关注人数
-        if (fellowedPeopleNumHtml != null) {
-            String fellowedPeopleNum = UtilsMethod.findFirstStringByRegex("被[0-9]+人关注",
-                fellowedPeopleNumHtml.text());
-            if (fellowedPeopleNum != null) {
-                fellowedPeopleNum = fellowedPeopleNum.replaceAll("[\\D]+", "");
-                if (fellowedPeopleNum != null) {
-                    info.setFellowedPeopleNum(fellowedPeopleNum);
-                } else {
-                    info.setFellowedPeopleNum(null);
-                }
-            } else {
-                info.setFellowedPeopleNum(null);
-            }
-        } else {
-            info.setFellowedPeopleNum(null);
+
+        //被关注的人数
+        String fellowedPeopleNum = getFellowedPeopleNum(doc);
+        info.setFellowedPeopleNum(fellowedPeopleNum);
+        if (fellowedPeopleNum == null) {
+            logger.infoLog("empty fellowed people");
         }
 
         //常去的小组
+        List<String> oftenGroup = getOftenGroup(doc);
+        info.setOftenGroup(oftenGroup);
+        if (oftenGroup == null) {
+            logger.infoLog("empty often group");
+        }
+
+        //获取在读的书列表
+        List<String> readingBook = getReadingBook(obssinHtml);
+        info.setReadingBook(readingBook);
+        if (readingBook == null) {
+            logger.infoLog("empty reading book");
+        }
+
+        //获取想读的书列表
+        List<String> wantBook = getWantBook(obssinHtml);
+        info.setWantBook(wantBook);
+        if (wantBook == null) {
+            logger.infoLog("empty want book");
+        }
+
+        //想看的电影
+        List<String> wantMovie = getWantMovie(obssinHtml);
+        info.setWantMovie(wantMovie);
+        if (wantMovie == null) {
+            logger.infoLog("empty want movie");
+        }
+
+        //看过的电影
+        List<String> watchedMovie = getWatchedMovie(obssinHtml);
+        info.setWatchedMovie(watchedMovie);
+        if (watchedMovie == null) {
+            logger.infoLog("empty watched movie");
+        }
+
+        //在听的歌
+        List<String> listeningMusic = getListeningMusic(obssinHtml);
+        info.setListeningMusic(listeningMusic);
+        if (listeningMusic == null) {
+            logger.infoLog("empty listening music");
+        }
+
+        //想听的歌
+        List<String> wantMusic = getWantMusic(obssinHtml);
+        info.setWantMusic(wantMusic);
+        if (wantMusic == null) {
+            logger.infoLog("empty want music");
+        }
+
+        //加入时间
+        String joinDate = getJoinDate(doc);
+        info.setJoinDate(joinDate);
+        if (joinDate == null) {
+            logger.infoLog("empty join date");
+        }
+        //性别（暂时无法获取，设置默认值）
+        info.setSex("n");
+        //年龄（暂时无法获取，设置默认值）
+        info.setAge("0");
+        //姓名
+        String name = getName(doc);
+        info.setUserName(name);
+        if (name == null) {
+            logger.infoLog("empty name");
+        }
+        
+        return info;
+    }
+
+    private List<String> getOftenGroup(Document doc) {
+        Elements oftenGroupHtml = doc.select("dl[class=\"ob\"] dd a");
         if (oftenGroupHtml != null) {
             List<String> oftenGroup = new ArrayList<String>();
             for (Element group : oftenGroupHtml) {
                 oftenGroup.add(group.text());
             }
-            info.setOftenGroup(oftenGroup);
+            return oftenGroup;
         } else {
-            info.setOftenGroup(null);
-            logger.infoLog("empty often group");
+            return null;
         }
+    }
 
-        //获取在读的书列表
-        if (readingBookHtml != null) {
-            List<String> readingBook = new ArrayList<String>();
-            for (Element book : readingBookHtml) {
-                readingBook.add(book.attr("alt"));
+    private List<String> getReadingBook(Elements obssinHtml) {
+        if (obssinHtml == null) {
+            return null;
+        }
+        for (Element books : obssinHtml) {
+            Elements substatus = books.select("div[class=\"substatus\"]");
+            if (substatus.text().equals("在读")) {
+                Elements readingBookHtml = books.select("li[class=\"aob\"] a img");
+                if (readingBookHtml != null) {
+                    List<String> readingBook = new ArrayList<String>();
+                    for (Element book : readingBookHtml) {
+                        readingBook.add(book.attr("alt"));
+                    }
+                    return readingBook;
+                } else {
+                    return null;
+                }
             }
-            info.setReadingBook(readingBook);
-        } else {
-            info.setReadingBook(null);
-            logger.infoLog("empty reading book");
-        }
-        //获取想读的书列表
-        if (wantBookHtml != null) {
-            List<String> wantBook = new ArrayList<String>();
-            for (Element book : wantBookHtml) {
-                wantBook.add(book.attr("alt"));
-            }
-            info.setWantBook(wantBook);
-        } else {
-            info.setWantBook(null);
-            logger.infoLog("empty want book");
-        }
-        //想看的电影
-        if (wantMovieHtml != null) {
-            List<String> wantMovie = new ArrayList<String>();
-            for (Element movie : wantMovieHtml) {
-                wantMovie.add(movie.attr("title"));
-            }
-            info.setWantMovie(wantMovie);
-        } else {
-            info.setWantMovie(null);
-            logger.infoLog("empty want movie");
-        }
-        //看过的电影
-        if (watchedMovieHtml != null) {
-            List<String> watchedMovie = new ArrayList<String>();
-            for (Element movie : watchedMovieHtml) {
-                watchedMovie.add(movie.attr("title"));
-            }
-            info.setWatchedMovie(watchedMovie);
-        } else {
-            info.setWatchedMovie(null);
-            logger.infoLog("empty watched movie");
-        }
-        //在听的歌
-        if (listeningMusicHtml != null) {
-            List<String> listeningMusic = new ArrayList<String>();
-            for (Element music : listeningMusicHtml) {
-                listeningMusic.add(music.attr("title"));
-            }
-            info.setListeningMusic(listeningMusic);
-        } else {
-            info.setListeningMusic(null);
-            logger.infoLog("empty listening music");
-        }
-        //想听的歌
-        if (wantMusicHtml != null) {
-            List<String> wantMusic = new ArrayList<String>();
-            for (Element music : wantMusicHtml) {
-                wantMusic.add(music.attr("title"));
-            }
-            info.setWantMusic(wantMusic);
-        } else {
-            info.setWantMusic(null);
-            logger.infoLog("empty want music");
-        }
-        //加入时间
-        String joinDate = UtilsMethod.findFirstStringByRegex("[0-9]{4}\\-[0-9]{2}\\-[0-9]{2}",
-            joinDateHtml.text());
-        if (joinDate != null) {
-            info.setJoinDate(joinDate);
-        } else {
-            logger.infoLog("empty join date");
-        }
-        //性别
-        info.setSex("n");
-        //年龄
-        info.setAge("0");
 
+
+
+
+        }
+        return null;
+    }
+
+    private List<String> getWantBook(Elements obssinHtml) {
+        if (obssinHtml == null) {
+            return null;
+        }
+        for (Element books : obssinHtml) {
+            Elements substatus = books.select("div[class=\"substatus\"]");
+            if (substatus.text().equals("想读")) {
+                Elements wantBookHtml = books.select("li[class=\"aob\"] a img");
+                if (wantBookHtml != null) {
+                    List<String> wantBook = new ArrayList<String>();
+                    for (Element book : wantBookHtml) {
+                        wantBook.add(book.attr("alt"));
+                    }
+                    return wantBook;
+                } else {
+                    return null;
+                }
+            }
+        }
+        return null;
+    }
+
+    private List<String> getWantMovie(Elements obssinHtml) {
+        if (obssinHtml == null) {
+            return null;
+        }
+        for (Element books : obssinHtml) {
+            Elements substatus = books.select("div[class=\"substatus\"]");
+            if (substatus.text().equals("想看")) {
+                Elements wantMovieHtml = books.select("li[class=\"aob\"] a");
+                if (wantMovieHtml != null) {
+                    List<String> wantMovie = new ArrayList<String>();
+                    for (Element movie : wantMovieHtml) {
+                        wantMovie.add(movie.attr("title"));
+                    }
+                    return wantMovie;
+                } else {
+                    return null;
+                }
+            }
+        }
+        return null;
+    }
+
+    private List<String> getWatchedMovie(Elements obssinHtml) {
+        if (obssinHtml == null) {
+            return null;
+        }
+        for (Element books : obssinHtml) {
+            Elements substatus = books.select("div[class=\"substatus\"]");
+            if (substatus.text().equals("看过")) {
+                Elements watchedMovieHtml = books.select("li[class=\"aob\"] a");
+                if (watchedMovieHtml != null) {
+                    List<String> watchedMovie = new ArrayList<String>();
+                    for (Element movie : watchedMovieHtml) {
+                        watchedMovie.add(movie.attr("title"));
+                    }
+                    return watchedMovie;
+                } else {
+                    return null;
+                }
+            }
+
+        }
+        return null;
+    }
+
+    private List<String> getListeningMusic(Elements obssinHtml) {
+        if (obssinHtml == null) {
+            return null;
+        }
+        for (Element books : obssinHtml) {
+            Elements substatus = books.select("div[class=\"substatus\"]");
+            if (substatus.text().equals("在听")) {
+                Elements listeningMusicHtml = books.select("li[class=\"aob\"] a");
+                if (listeningMusicHtml != null) {
+                    List<String> listeningMusic = new ArrayList<String>();
+                    for (Element music : listeningMusicHtml) {
+                        listeningMusic.add(music.attr("title"));
+                    }
+                    return listeningMusic;
+                } else {
+                    return null;
+                }
+            }
+
+        }
+        return null;
+    }
+
+    private List<String> getWantMusic(Elements obssinHtml) {
+        if (obssinHtml == null) {
+            return null;
+        }
+        for (Element books : obssinHtml) {
+            Elements substatus = books.select("div[class=\"substatus\"]");
+            if (substatus.text().equals("想听")) {
+                Elements wantMusicHtml = books.select("li[class=\"aob\"] a");
+                if (wantMusicHtml != null) {
+                    List<String> wantMusic = new ArrayList<String>();
+                    for (Element music : wantMusicHtml) {
+                        wantMusic.add(music.attr("title"));
+                    }
+                    return wantMusic;
+                } else {
+                    return null;
+                }
+            }
+
+        }
+        return null;
+    }
+
+    /**
+     * achieve the num of people him/her fellowed
+     * @param doc
+     * @return
+     */
+    private String getFellowPeopleNum(Document doc) {
+        Elements friendHtml = doc.select("div[id=\"friend\"]");
+        Elements fellowPeopleNumHtml = null;
+
+        if (friendHtml != null) {
+            fellowPeopleNumHtml = friendHtml.select("a");
+            //关注人数
+            if (fellowPeopleNumHtml != null) {
+                String fellowPeopleNum = UtilsMethod.findFirstStringByRegex("成员[0-9]+",
+                    fellowPeopleNumHtml.text());
+                if (fellowPeopleNum != null) {
+                    fellowPeopleNum = fellowPeopleNum.replaceAll("[\\D]+", "");
+                    if (fellowPeopleNum != null) {
+                        return fellowPeopleNum;
+                    } else {
+                        return null;
+                    }
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * achieve the num of people fellow him/her
+     * @param doc
+     * @return
+     */
+    private String getFellowedPeopleNum(Document doc) {
+        Elements friendHtml = doc.select("div[id=\"friend\"]");
+        Elements fellowedPeopleNumHtml = null;
+        if (friendHtml != null) {
+            fellowedPeopleNumHtml = friendHtml.select("p[class=\"rev-link\"]");
+            //被关注人数
+            if (fellowedPeopleNumHtml != null) {
+                String fellowedPeopleNum = UtilsMethod.findFirstStringByRegex("被[0-9]+人关注",
+                    fellowedPeopleNumHtml.text());
+                if (fellowedPeopleNum != null) {
+                    fellowedPeopleNum = fellowedPeopleNum.replaceAll("[\\D]+", "");
+                    if (fellowedPeopleNum != null) {
+                        return fellowedPeopleNum;
+                    } else {
+                        return null;
+                    }
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * achieve the person name on douban
+     * @param doc
+     * @return
+     */
+    private String getName(Document doc) {
+        Elements nameHtml = doc.select("title");
         //姓名
         if (nameHtml != null) {
-            info.setUserName(nameHtml.text());
+            return nameHtml.text();
         } else {
-            logger.infoLog("empty name");
+            return null;
         }
-        
+    }
 
-        return info;
+    /**
+     * achieve the person join douban date
+     * @param doc
+     * @return
+     */
+    private String getJoinDate(Document doc) {
+        Elements joinDateHtml = doc.select("div[class=\"user-info\"] div[class=\"pl\"]");
+        if (joinDateHtml != null) {
+            return UtilsMethod.findFirstStringByRegex("[0-9]{4}\\-[0-9]{2}\\-[0-9]{2}",
+                joinDateHtml.text());
+        } else {
+            return null;
+        }
     }
 
 }
